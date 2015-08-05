ca2e1c91731c4b57e5c979c7773a7cdc102abdf7
Greg Denehy <greg@inclose.com.au>
2015-08-05 21:47:42 +0930

    First pages commit

---
+++ plugin/menu/README.md
@@ -0,0 +1,127 @@
+# reveal.js-menu
+
+A slideout menu plugin for [Reveal.js](https://github.com/hakimel/reveal.js) to quickly jump to any slide by title. Also optionally change the theme and set the default transition.
+
+## Installation
+
+Copy this repository into the plugins folder of your reveal.js presentation, ie ```plugins/menu```.
+
+Add the plugin to the dependencies in your presentation, as below. 
+
+```javascript
+Reveal.initialize({
+	// ...
+	
+	dependencies: [
+		// ... 
+	  
+		{ src: 'plugin/menu/menu.js' }
+	]
+});
+```
+
+## Configuration
+
+You can configure the menu for your presentation by providing a ```menu``` option in the reveal.js initialization options. Note that all config values are optional and will default as specified below.
+
+
+```javascript
+Reveal.initialize({
+	// ...
+
+	menu: {
+		// Specifies which side of the presentation the menu will 
+		// be shown. Use 'left' or 'right'.
+		side: 'left',
+
+		// Add slide numbers to the titles in the slide list.
+		// Use 'true' or format string (same as reveal.js slide numbers)
+		numbers: false,
+
+		// Add markers to the slide titles to indicate the 
+		// progress through the presentation
+		markers: false,
+
+		// Specifies the themes that will be available in the themes
+		// menu panel. Set to 'false' to hide themes panel.
+		themes: [
+			{ name: 'Black', theme: 'css/theme/black.css' },
+			{ name: 'White', theme: 'css/theme/white.css' },
+			{ name: 'League', theme: 'css/theme/league.css' },
+			{ name: 'Sky', theme: 'css/theme/sky.css' },
+			{ name: 'Beige', theme: 'css/theme/beige.css' },
+			{ name: 'Simple', theme: 'css/theme/simple.css' },
+			{ name: 'Serif', theme: 'css/theme/serif.css' },
+			{ name: 'Blood', theme: 'css/theme/blood.css' },
+			{ name: 'Night', theme: 'css/theme/night.css' },
+			{ name: 'Moon', theme: 'css/theme/moon.css' },
+			{ name: 'Solarized', theme: 'css/theme/solarized.css' }
+		],
+
+		// Specifies if the transitions menu panel will be shown.
+		transitions: true,
+
+		// Adds a menu button to the slides to open the menu panel.
+		// Set to 'false' to hide the button.
+		openButton: true,
+
+		// If 'true' allows the slide number in the presentation to
+		// open the menu panel. The reveal.js slideNumber option must 
+		// be displayed for this to take effect
+		openSlideNumber: false
+	},
+
+});
+```
+
+## Slide Titles
+
+The slide titles used in the menu can be supplied explicitly or are taken directly from the presentation, using the following rules...
+
+###### 1. The section's ```data-menu-title``` attribute.
+If the slide's section element contains a ```data-menu-title``` attribute this will be used for the slide title in the menu. For example...
+
+```html
+<section data-menu-title="Custom Menu Title">
+	<h1>Title</h1>
+	<p>...</p>
+</section>
+```
+
+###### 2. Any element with the class ```menu-title```.
+If the slide's section contains an element with the class ```menu-title``` then the element's text will be used for the title. The first such element found will be used if there are more than one. Note the element need not be displayed to be used. For example...
+
+```html
+<section>
+	<h1>Title</h1>
+	<span class="menu-title" style="display: none">Custom Menu Title</span>
+	<p>...</p>
+</section>
+```
+
+###### 3. The first heading found
+If not explicitly specified (as above), the title will be taken from the first heading element found in the slide. For example...
+
+```html
+<section>
+	<h3>This will be the slide title in the menu</h3>
+	<h1>Title</h1>
+	<p>...</p>
+</section>
+```
+
+###### 4. No title is provided
+If no title can be found using the above methods, a default title incorporating the slide number will be used. For example, the following would result in a slide title in the format of 'Slide 12'...
+
+```html
+<section>
+	<p>...</p>
+</section>
+```
+
+
+## License
+
+MIT licensed
+
+Copyright (C) 2015 Greg Denehy
---
+++ plugin/menu/menu.js
@@ -0,0 +1,315 @@
+/*
+ * Reveal.js menu plugin
+ * MIT licensed
+ * (c) Greg Denehy 2015
+ */
+
+var RevealMenu = window.RevealMenu || (function(){
+	var options = Reveal.getConfig().menu || {};
+	options.path = options.path || 'plugin/menu';
+
+	var module = {};
+
+	loadResource(options.path + '/lib/jeesh.min.js', 'script', function() {
+	loadResource(options.path + '/lib/bowser.min.js', 'script', function() {
+	loadResource(options.path + '/menu.css', 'stylesheet', function() {
+	loadResource(options.path + '/font-awesome-4.3.0/css/font-awesome.min.css', 'stylesheet', function() {
+		// does not support IE8 or below
+		if (!bowser.msie || bowser.version >= 9) {
+			//
+			// Utilty functions
+			//
+
+			function openMenu(event) {
+				if (event) event.preventDefault();
+			    $('body').addClass('slide-menu-active');
+			    $('.reveal').addClass('has-' + options.effect + '-' + options.side);
+			    $('.slide-menu').addClass('active');
+			    $('.slide-menu-overlay').addClass('active');
+
+			    // set highlight for selected theme
+			    $('div[data-panel="Themes"] li').removeClass('active');
+			    $('li[data-theme="' + $('#theme').attr('href') + '"]').addClass('active');
+
+			    // set hightlight for selected transition
+			    $('div[data-panel="Transitions"] li').removeClass('active');
+			    $('li[data-transition="' + Reveal.getConfig().transition + '"]').addClass('active');
+			}
+
+			function closeMenu(event) {
+				if (event) event.preventDefault();
+			    $('body').removeClass('slide-menu-active');
+			    $('.reveal').removeClass('has-' + options.effect + '-' + options.side);
+			    $('.slide-menu').removeClass('active');
+			    $('.slide-menu-overlay').removeClass('active');
+			}
+
+			function toggleMenu(event) {
+				if ($('body').hasClass('slide-menu-active')) {
+					closeMenu(event);
+				} else {
+					openMenu(event);
+				}
+			}
+
+			function openPanel(event) {
+				openMenu(event);
+				var panel = $(event.currentTarget).data('panel');
+				$('.slide-menu-toolbar > li').removeClass('active-toolbar-button');
+				$('li[data-panel="' + panel + '"]').addClass('active-toolbar-button');
+				$('.slide-menu-panel').removeClass('active-menu-panel');
+				$('div[data-panel="' + panel + '"]').addClass('active-menu-panel');
+			}
+
+
+			options.side = options.side || 'left';	// 'left' or 'right'
+			options.numbers = options.numbers || false;
+			options.markers = options.markers || false;
+			if (typeof options.themes === "undefined") {
+				options.themes = [
+					{ name: 'Black', theme: 'css/theme/black.css' },
+					{ name: 'White', theme: 'css/theme/white.css' },
+					{ name: 'League', theme: 'css/theme/league.css' },
+					{ name: 'Sky', theme: 'css/theme/sky.css' },
+					{ name: 'Beige', theme: 'css/theme/beige.css' },
+					{ name: 'Simple', theme: 'css/theme/simple.css' },
+					{ name: 'Serif', theme: 'css/theme/serif.css' },
+					{ name: 'Blood', theme: 'css/theme/blood.css' },
+					{ name: 'Night', theme: 'css/theme/night.css' },
+					{ name: 'Moon', theme: 'css/theme/moon.css' },
+					{ name: 'Solarized', theme: 'css/theme/solarized.css' }
+				];
+			}
+			if (typeof options.transitions === "undefined") options.transitions = true;
+			if (bowser.msie && bowser.version <= 9) {
+				// transitions aren't support in IE9 anyway, so no point in showing them
+				options.transitions = false;
+			}
+			if (typeof options.openButton === "undefined") options.openButton = true;
+			if (typeof options.openSlideNumber === "undefined") options.openSlideNumber = false;
+
+			$('<nav class="slide-menu slide-menu--' + options.side + '"></nav>')
+				.appendTo($('.reveal'));
+			$('<div class="slide-menu-overlay"></div>')
+				.appendTo($('.reveal'))
+				.click(closeMenu);
+
+			var toolbar = $('<ol class="slide-menu-toolbar"></ol>').prependTo($('.slide-menu'));
+			$('<li data-panel="Slides"><span class="slide-menu-toolbar-label">Slides</span><br/><i class="fa fa-list"></i></li>')
+				.appendTo(toolbar)
+				.addClass('active-toolbar-button')
+				.click(openPanel);
+			if (options.themes) {
+				$('<li data-panel="Themes"><span class="slide-menu-toolbar-label">Themes</span><br/><i class="fa fa-desktop"></i></li>')
+					.appendTo(toolbar)
+					.click(openPanel);
+			}
+			if (options.transitions) {
+				$('<li data-panel="Transitions"><span class="slide-menu-toolbar-label">Transitions</span><br/><i class="fa fa-arrows-h"></i></li>')
+					.appendTo(toolbar)
+					.click(openPanel);
+			}
+			$('<li id="close"><span class="slide-menu-toolbar-label">Close</span><br/><i class="fa fa-times"></i></li>')
+				.appendTo(toolbar)
+				.click(closeMenu);
+
+			var panels = $('<div class="slide-menu-panels"></div>').appendTo($('.slide-menu'));
+
+			//
+			// Slide links
+			//
+			function item(type, section, i, h, v) {
+				var link = '/#/' + h;
+				if (v) link += '/' + v;
+				else v = 0;
+
+				var title = $(section).data('menu-title') ||
+					$('.menu-title', section).text() ||
+					$('h1, h2, h3, h4, h5, h6', section).text();
+				if (!title) {
+					title = "Slide " + i;
+					type += ' no-title';
+				}
+
+				title = '<span class="slide-menu-item-title">' + title + '</span>';
+				if (options.numbers) {
+					// Number formatting taken from reveal.js
+
+					// Default to only showing the current slide number
+					var format = 'c';
+
+					// Check if a custom slide number format is available
+					if( typeof options.numbers === 'string' ) {
+						format = options.numbers;
+					}
+
+					var n = format.replace( /h/g, h )
+									.replace( /v/g, v )
+									.replace( /c/g, i )
+									.replace( /t/g, Reveal.getTotalSlides() );
+
+					title = '<span class="slide-menu-item-number">' + n + '. </span>' + title;
+				}
+
+				var markers = '';
+				if (options.markers) {
+					markers = '<i class="fa fa-check-circle past"></i>' +
+								'<i class="fa fa-dot-circle-o present"></i>' + 
+								'<i class="fa fa-circle-thin future"></i>';
+				}
+
+				return '<li class="' + type + '" data-slide-h="' + h + '" data-slide-v="' + v + '">' + markers + title + '</li>';
+			}
+
+			function clicked(event) {
+				event.preventDefault();
+				var elem = $(event.currentTarget);
+				Reveal.slide(elem.data('slide-h'), elem.data('slide-v'));
+				closeMenu();
+			}
+
+			function highlightCurrentSlide() {
+				var state = Reveal.getState();
+				$('li.slide-menu-item, li.slide-menu-item-vertical')
+					.removeClass('past')
+					.removeClass('present')
+					.removeClass('future');
+
+				$('li.slide-menu-item, li.slide-menu-item-vertical').each(function(e) {
+					var h = $(e).data('slide-h');
+					var v = $(e).data('slide-v');
+					if (h < state.indexh || (h === state.indexh && v < state.indexv)) {
+						$(e).addClass('past');
+					}
+					else if (h === state.indexh && v === state.indexv) {
+						$(e).addClass('present');
+					}
+					else {
+						$(e).addClass('future');
+					}
+				});
+			}
+
+			$('<div data-panel="Slides" class="slide-menu-panel"><ul class="slide-menu-items"></ul></div>')
+				.appendTo(panels)
+				.addClass('active-menu-panel');
+			var items = $('.slide-menu-items');
+			var count = 0;
+			$('.slides > section').each(function(section, h) {
+				var subsections = $('section', section);
+				if (subsections.length > 0) {
+					subsections.each(function(subsection, v) {
+						count++;
+						var type = (v === 0 ? 'slide-menu-item' : 'slide-menu-item-vertical');
+						items.append(item(type, subsection, count, h, v));
+					});
+				} else {
+					count++;
+					var type = 'slide-menu-item';
+					items.append(item(type, section, count, h));
+				}
+			});
+			$('.slide-menu-item, .slide-menu-item-vertical').click(clicked);
+
+			Reveal.addEventListener('slidechanged', highlightCurrentSlide);
+			highlightCurrentSlide();
+
+			//
+			// Themes
+			//
+			function setTheme(event) {
+				if (event) event.preventDefault();
+				$('#theme').attr('href', $(event.currentTarget).data('theme'));
+				closeMenu();
+			}
+
+			if (options.themes) {
+				var panel = $('<div data-panel="Themes" class="slide-menu-panel"></div>').appendTo(panels);
+				options.themes.forEach(function(t) {
+					$('<li data-theme="' + t.theme + '">' + t.name + '</li>').appendTo(panel).click(setTheme);
+				})
+			}
+
+			//
+			// Transitions
+			//
+			function setTransition(event) {
+				if (event) event.preventDefault();
+				Reveal.configure({ transition: $(event.currentTarget).data('transition') });
+				closeMenu();
+			}
+
+			if (options.transitions) {
+				var panel = $('<div data-panel="Transitions" class="slide-menu-panel"></div>')
+					.appendTo(panels);
+				$('<li data-transition="none">None</li>').appendTo(panel).click(setTransition);
+				$('<li data-transition="fade">Fade</li>').appendTo(panel).click(setTransition);
+				$('<li data-transition="slide">Slide</li>').appendTo(panel).click(setTransition);
+				$('<li data-transition="convex">Convex</li>').appendTo(panel).click(setTransition);
+				$('<li data-transition="concave">Concave</li>').appendTo(panel).click(setTransition);
+				$('<li data-transition="zoom">Zoom</li>').appendTo(panel).click(setTransition);
+			}
+
+			//
+			// Open menu options
+			//
+			if (options.openButton) {
+				// add menu button
+				$('<div class="slide-menu-button"><a href="#"><i class="fa fa-bars"></i></a></div>')
+					.appendTo($('.reveal'))
+					.click(openMenu);
+			}
+
+			if (options.openSlideNumber) {
+				// wrap slide number in link
+				$('<div class="slide-number-wrapper"><a href="#"></a></div>').insertAfter($('div.slide-number'));
+				$('.slide-number').appendTo($('.slide-number-wrapper a'));
+				$('.slide-number-wrapper a').click(openMenu);
+			}
+
+			module.toggle = toggleMenu;
+		}
+	})
+	})
+	})
+	});
+
+	// modified from math plugin
+	function loadResource( url, type, callback ) {
+		var head = document.querySelector( 'head' );
+		var resource;
+
+		if ( type === 'script' ) {
+			resource = document.createElement( 'script' );
+			resource.type = 'text/javascript';
+			resource.src = url;
+		}
+		else if ( type === 'stylesheet' ) {
+			resource = document.createElement( 'link' );
+			resource.rel = 'stylesheet';
+			resource.href = url;
+		}
+
+		// Wrapper for callback to make sure it only fires once
+		var finish = function() {
+			if( typeof callback === 'function' ) {
+				callback.call();
+				callback = null;
+			}
+		}
+
+		resource.onload = finish;
+
+		// IE
+		resource.onreadystatechange = function() {
+			if ( this.readyState === 'loaded' ) {
+				finish();
+			}
+		}
+
+		// Normal browsers
+		head.appendChild( resource );
+	}
+
+	return module;
+})();
