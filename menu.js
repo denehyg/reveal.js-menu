54a2ac524dc7d453fab4dc6d77b6f40fb7babf51
Greg Denehy <greg@inclose.com.au>
2017-11-15 09:32:34 +1030

    Added String.protoype.startsWith polyfill for IE

--- menu.js
+++ menu.js
@@ -857,5 +857,12 @@ var RevealMenu = window.RevealMenu || (function(){
 		return path;
 	}
 
+	// polyfill
+	if (!String.prototype.startsWith) {
+		String.prototype.startsWith = function(searchString, position){
+		  return this.substr(position || 0, searchString.length) === searchString;
+	  };
+	}
+
 	return module;
 })();
